81dcea775c8044bf233a430552dca7b7470e1389
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-08 00:01:22 +0530

    User Reg With Exception UC12

---
+++ src/main/java/com/bridgelab/userregexception/UserRegistrationException.java
@@ -0,0 +1,22 @@
+package com.bridgelab.userregexception;
+
+public class UserRegistrationException extends Throwable {
+
+	public ExceptionType exceptionType;
+	public String message;
+
+	public enum ExceptionType {
+		EMPTY, NULL, INVALID;
+	}
+
+	public UserRegistrationException(ExceptionType exceptionType, String message) {
+		this.exceptionType = exceptionType;
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "UserRegistrationException{" + "exceptionType=" + exceptionType + ", message='" + message + '\'' + '}';
+	}
+
+}
